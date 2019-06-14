a1d5fae23ffd5fc17cba311f4217a43ac3d57fb9
Aiden Boudreau <aiden.boudreau@outlook.com>
2019-06-14 11:46:06 -0500

    Added tableViewContoller methods and hard coded some things.

--- To-Do-It/TodoListViewController.swift
+++ To-Do-It/TodoListViewController.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Find Jeff", "Eat", "Milk", "Sleep"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-
+    //MARK - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+        
+        print(itemArray[indexPath.row])
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 
 }
 
