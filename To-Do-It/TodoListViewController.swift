3376d826993a993a3511ca5d9ba05f73598a1d97
Aiden Boudreau <aiden.boudreau@outlook.com>
2019-06-14 20:05:22 -0500

    Adding Core Data and app saves new, user added todos.

--- To-Do-It/TodoListViewController.swift
+++ To-Do-It/TodoListViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = ["Find Jeff", "Eat", "Milk", "Sleep"]
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
+        
     }
     
     //MARK - TableView Datasource Methods
@@ -59,6 +65,8 @@ class TodoListViewController: UITableViewController {
         let addAction = UIAlertAction(title: "Add", style: .default) { (action) in
             
             self.itemArray.append(newItemName.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
             
